0089eb161af4ca463a7a702f575bc7413fb687cb
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-10-01 22:02:01 +0600

    delete scaffold template

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -24,64 +24,6 @@ export interface BookElem {
   providedIn: 'root'
 })
 export class DataService {
-  public messages: Message[] = [
-    {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
-      id: 1,
-      read: false
-    },
-    {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
-      id: 2,
-      read: false
-    },
-    {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
-      id: 3,
-      read: false
-    },
-    {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 7,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 8,
-      read: false
-    }
-  ];
   public books: BookElem[] = [
     {
       author: 'Harper Lee',
@@ -276,17 +218,10 @@ export class DataService {
   public detectChange$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
   constructor() {
+    // here will be load from websql /\=\
   }
 
   private idLast = 28;
-
-  public getMessages(): Message[] {
-    return this.messages;
-  }
-
-  public getMessageById(id: number): Message {
-    return this.messages[id];
-  }
 
   public getBooks(): BookElem[] {
     return this.books;
